3017c703ca3753357a936f0d044bd818085df258
crmankin <crmankin@franklincountyohio.gov>
2022-09-24 20:09:22 -0400

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import AddNameForm from './components/AddNameForm'
 import Phonebook from './components/Phonebook'
+import NameFilter from './components/NameFilter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-7890' }
+    { id: 1, name: 'Christopher Mankin', number: '419-346-3470' },
+    { id: 2, name: 'Jack Mankin Sr', number: '614-491-6636' },
+    { id: 3, name: 'Kevin Schultz', number: '563-881-9123' },
+    { id: 4, name: 'Michelle Callahan', number: '888-888-8888' }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [nameFilter, setNameFilter] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -15,7 +20,11 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
+
+  const handleNameFilterChange = (event) => {
+    setNameFilter(event.target.value.toLowerCase());
+  };
 
   const handleAddButton = (event) => {
     event.preventDefault();
@@ -42,7 +51,8 @@ const App = () => {
         handleNumberChange={handleNumberChange}
         handleAddButton={handleAddButton} />
       <h2>Phone Book</h2>
-      <Phonebook persons={persons} />
+      <NameFilter nameFilter={nameFilter} handleNameFilterChange={handleNameFilterChange} />
+      <Phonebook persons={persons} nameFilter={nameFilter} />
     </div>
   );
 
--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,10 +1,14 @@
-export const Phonebook = ({ persons }) => {
+export const Phonebook = ({ persons, nameFilter }) => {
   if (persons.length === 0)
     return <div>Your Phonebook is currently empty.</div>
 
+  const shownPersons = nameFilter
+    ? persons.filter(e => e.name.toLowerCase().includes(nameFilter))
+    : persons;
+
   return (
     <ul>
-      {persons.map(p => <li key={p.name}>{p.name}: {p.number}</li>)}
+      {shownPersons.map(p => <li key={p.id}>{p.name}: {p.number}</li>)}
     </ul>
   );
 };
