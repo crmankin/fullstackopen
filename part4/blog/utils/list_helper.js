e5f34ad7a355eaf2f3ec5b0b5f665ce83f2fe507
crmankin <crmankin@franklincountyohio.gov>
2022-10-11 09:03:06 -0400

    Exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,14 +1,27 @@
 
-const dummy = (blogs) => {
-    return 1;
-};
-
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, next) => sum + next.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    if (!blogs || blogs.length === 0) return null;
+
+    let maxLikes = -1;
+    let fb = null;
+
+    for (let i in blogs) {
+        let b = blogs[i];
+        if (b.likes > maxLikes) {
+            fb = b;
+            maxLikes = b.likes;
+        }
+    }
+
+    return fb;
+};
+
 
 module.exports = {
-    dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
