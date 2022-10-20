3100ed84664615ea49f11fde7c31d8c910de8e2b
crmankin <crmankin@franklincountyohio.gov>
2022-10-20 15:51:40 -0400

    Exercise 5.9

--- part5/blog-frontend/src/components/BlogList.js
+++ part5/blog-frontend/src/components/BlogList.js
@@ -2,10 +2,12 @@ import Blog from './Blog'
 
 const BlogList = ({ blogs, handleLike }) => {
 
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
+
   return (
     <div>
       <h2>Blogs</h2>
-      {blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} handleLike={handleLike} />
       )}
     </div>
